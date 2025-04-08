99682e38bc4539987221b6cfe16d24fa24c4ec57
Abra7abra7 <stancikmarian8@gmail.com>
2025-04-08 15:35:37 +0200

    fix

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error('STRIPE_SECRET_KEY environment variable is not set.');
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-06-20', // Použi aktuálnu verziu API
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2025-03-31.basil', // Aktualizovaná verzia API podľa typov
   typescript: true,
 });
